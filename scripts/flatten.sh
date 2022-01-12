afd3cc38066b2c78253d5e1811392a3d0d9bba17
tarassh <tshchybo@gmail.com>
2022-01-12 11:01:24 +0200

    fix flattener

--- scripts/flatten.sh
+++ scripts/flatten.sh
@@ -4,14 +4,14 @@ rm -rf temp
 mkdir -p temp
 
 cd contracts
-find . -type f -name "*.sol" -exec sed -i 's/pragma solidity 0.7.6/pragma solidity ^0.7.6/g' {} +
+find . -type f -name "*.sol" -exec sed -i 's/pragma solidity 0.8.11/pragma solidity ^0.8.11/g' {} +
 cd ..
 
-alias flatten="./node_modules/.bin/solidity-flattener"
+alias flatten="./node_modules/.bin/truffle-flattener"
 
 flatten contracts/NFT721.sol --output temp/NFT721.sol
 flatten contracts/NFT1155.sol --output temp/NFT1155.sol
 
 cd contracts
-find . -type f -name "*.sol" -exec sed -i 's/pragma solidity ^0.7.6/pragma solidity 0.7.6/g' {} +
+find . -type f -name "*.sol" -exec sed -i 's/pragma solidity ^0.8.11/pragma solidity 0.8.11/g' {} +
 cd ..
