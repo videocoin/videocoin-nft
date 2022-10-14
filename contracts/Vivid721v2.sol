2c45e19a1d63be747f0a0cf064e0066dfe9a8500
--local <--local>
2022-10-14 13:09:11 -0400

    fix opensea issue

--- contracts/Vivid721v2.sol
+++ contracts/Vivid721v2.sol
@@ -6,43 +6,51 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Vivid721v2 is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, AccessControl {
+contract Vivid721v2 is
+    ERC721,
+    ERC721Enumerable,
+    ERC721URIStorage,
+    ERC721Burnable,
+    AccessControl,
+    Ownable
+{
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant UPDATER_ROLE = keccak256("UPDATER_ROLE");
 
-    constructor(string memory name, string memory symbol)
-        ERC721(name, symbol) 
-    {
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
         _grantRole(UPDATER_ROLE, msg.sender);
     }
 
-    function mint(address to, uint256 tokenId, string memory uri) 
-        public
-        onlyRole(MINTER_ROLE) 
-    {
+    function mint(
+        address to,
+        uint256 tokenId,
+        string memory uri
+    ) public onlyRole(MINTER_ROLE) {
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
-    function updateTokenURI(uint256 tokenId, string memory uri) 
-        public 
+    function updateTokenURI(uint256 tokenId, string memory uri)
+        public
         onlyRole(UPDATER_ROLE)
     {
-		_setTokenURI(tokenId, uri);
-	}
+        _setTokenURI(tokenId, uri);
+    }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override(ERC721, ERC721Enumerable) 
-    {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
     function _burn(uint256 tokenId)
-        internal 
+        internal
         override(ERC721, ERC721URIStorage)
     {
         super._burn(tokenId);
